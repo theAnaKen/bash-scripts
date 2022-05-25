7cc11f86ec3c9cb2a466b58ac28e9b2921fbed0a
theAnaKen <the.anaken@gmail.com>
2022-05-25 20:38:54 +0530

    formatted code better

--- eldenRing.sh
+++ eldenRing.sh
@@ -8,21 +8,21 @@ echo "Welcome Tarnished. Please select your starting class:
 read class
 
 case $class in
-	1)
-		type="Samurai"
-		hp=10
-		attack=20
-		;;
-	2) 
-		type="Prisoner"
-		hp=20
-		attack=4
-		;;
-	3) 
-		type="Prophet"
-		hp=30
-		attack=4
-		;;
+1)
+	type="Samurai"
+	hp=10
+	attack=20
+	;;
+2)
+	type="Prisoner"
+	hp=20
+	attack=4
+	;;
+3)
+	type="Prophet"
+	hp=30
+	attack=4
+	;;
 esac
 
 echo "You have chosen $type class. Your hp is $hp and your attack is $attack"
@@ -31,11 +31,11 @@ echo "You Died"
 
 #First beast battle
 #-------------------
-bossValue=$(( $RANDOM % 2 ))
+bossValue=$(($RANDOM % 2))
 
 echo "Your first beast approaches... Prepare to battle. Pick a no b/w 0 and 1 (0/1)"
 read Tarnished
-if [[ $Tarnished == $SudoPASS ]] ; then
+if [[ $Tarnished == $SudoPASS ]]; then
 	echo "Hey, $USER is root, he always wins."
 elif [[ $bossValue == $Tarnished ]]; then
 	echo "Beast VANQUISHED! Congrats fellow Tarnished"
@@ -47,12 +47,12 @@ fi
 sleep 2
 
 #Boss Battle
-Marget=$(( $RANDOM % 10 ))
+Marget=$(($RANDOM % 10))
 
 echo "BOSS BATTLE INCOMMING!! Marget approaches... Pick a number b/w 0 - 9 to battle (0-9)"
 read Tarnished
 if [[ $Marget == $Tarnished || $Tarnished == "cheat" ]]; then
-        echo "Beast VANQUISHED! Congrats fellow Tarnished"
+	echo "Beast VANQUISHED! Congrats fellow Tarnished"
 elif [[ $Tarnished == $SudoPASS ]]; then
 	echo "Hey $USER is root. He always wins"
 else
